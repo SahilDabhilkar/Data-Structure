92a7c0d66a1336802bff66531e53a78297d749c6
Sahil Dabhilkar <92622245+SahilDabhilkar@users.noreply.github.com>
2022-08-24 21:15:13 +0530

    Array code
    Array code this is contain Insert , delete , update, creation,display.

---
+++ array.c
@@ -0,0 +1,86 @@
+//
+//  main.c
+//  collage
+//
+//  Created by Sahil Dabhilkar on 24/08/22.
+//
+
+#include <stdio.h>
+int a[10];
+int i;
+int size;
+int pos;
+int value;
+
+int insertElement(void);
+int viewElement(void);
+int addElement(void);
+int updateElement(void);
+int deleteElement(void);
+
+
+int main() {
+    
+    addElement();
+   // insertElement();
+  //  updateElement();
+    viewElement();
+    deleteElement();
+    viewElement();
+    
+    return 0;
+}
+
+
+int insertElement()
+{
+        printf("Enter the position :");
+        scanf("%d",&pos);
+        printf("Enter the number :");
+        scanf("%d",&value);
+            a[pos-1]=value;
+    return 0;
+}
+
+
+int viewElement()
+{
+    for (i=0; i<size; i++) {
+        printf("%d\t",a[i]);
+    }
+    return 0;
+}
+
+
+int addElement()
+{
+    printf("Enter the size :");
+    scanf("%d",&size);
+    for (i=0; i<size; i++) {
+        printf("Enter the Element :");
+        scanf("%d",&a[i]);
+    }
+    return 0;
+}
+
+
+int updateElement()
+{
+    printf("Enter the postion you want to update :");
+    scanf("%d",&pos);
+    printf("Enter the number :");
+    scanf("%d",&value);
+    a[pos-1]=value;
+    return 0;
+}
+
+
+int deleteElement()
+{
+    printf("Enter the postion you want to delete :");
+    scanf("%d",&pos);
+    for (i=pos; i<=size; i++) {
+        a[i]=a[i+1];
+    }
+    return 0;
+}
